51e3fc8060fa17066cab2b7e13c640829c98f47c
Akinnuoye Tolulope I <myvisionisvalid@gmail.com>
2020-03-03 15:47:44 +0100

    using vue-router "npm install vue-router --save"

--- src/main.js
+++ src/main.js
@@ -3,16 +3,39 @@
 import Vue from 'vue'
 import App from './App'
 import vueResource from 'vue-resource'
-// import router from './router'
+import vueRouter from 'vue-router'
+
+import Users from './components/user'
+import Test from './components/test'
 
 Vue.use(vueResource)
+Vue.use(vueRouter)
+
+const router = new vueRouter({
+  mode: 'history', //to be able no navigate directly to url
+  base: __dirname,
+  routes: [
+    {path:'/', component: Users},
+    {path:'/test', component: Test},
+  ]
+});
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  // router,
-  components: { App },
-  template: '<App/>'
-})
+  // el: '#app',
+  // // router,
+  // components: { App },
+  // template: '<App/>'
+  router,
+  template: `
+    <div id="app">
+      <ul>
+        <li><router-link to="/">Users</router-link></li>
+        <li><router-link to="/test">Test</router-link></li>
+      </ul>
+      <router-view></router-view>
+    </div>
+  `
+}).$mount('#app')
